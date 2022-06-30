fcb9a4e1f7516dced7cf11a1ea4b229052187870
unknown <lukpsdn@gmail.com>
2022-06-30 15:05:10 -0300

    JS Contador

--- JS/assets/js/scripts.js
+++ JS/assets/js/scripts.js
@@ -2,19 +2,23 @@ let count = 0;
 
 const CURRENT_NUMBER = document.getElementById('currentNumber');
 
+function mudar_cor() {
+    if (count >= 0){
+        CURRENT_NUMBER.style.color = 'blue'
+        }
+    else if(count <0){
+        CURRENT_NUMBER.style.color = 'red'
+    }
+}
+
 function increment(){
     if(count >= 10){
         alert("Você atingiu o limite máximo")
     } else {
         count++;
         CURRENT_NUMBER.innerHTML = count;
-    if(count >= 0){
-        CURRENT_NUMBER.style.color = 'blue'
-        }
-    else if(count < 0){
-        CURRENT_NUMBER.style.color = 'red'
-        }
     }
+    mudar_cor();
 }
 
 function decrement(){
@@ -23,17 +27,6 @@ function decrement(){
     } else {
         count--;
         CURRENT_NUMBER.innerHTML = count;
-    if (count >= 0){
-        CURRENT_NUMBER.style.color = 'blue'
-        }
-    else if(count <0){
-        CURRENT_NUMBER.style.color = 'red'
-        }
     }
-}
-
-function negativo() {
-    if(count < 0){
-        alert("Número é negativo");
-    }
+    mudar_cor();
 }
